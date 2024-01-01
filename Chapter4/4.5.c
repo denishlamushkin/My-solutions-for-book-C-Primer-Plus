44c69b642673c45653f933d21039d08fc2d0db33
Denis Hlamushkin <rei.0509hl@gmail.com>
2024-01-01 23:21:16 +0500

    Update 4.5.c

--- Chapter4/4.5.c
+++ Chapter4/4.5.c
@@ -5,11 +5,11 @@ int main(void)
 {
     float speed; // скорость загрузки в мегабитах
     float weight; // размер файла в мегабайтах
-    printf("Введите скорость загрузки в мегабитах в секунду.\n");
+    printf("Enter speed  of download in Mbit in second.\n");
     scanf("%f", &speed);
-    printf("Введите размер файла в мегабайтах.\n");
+    printf("Enter weight of the file in Mbait.\n");
     scanf("%f", &weight);
-    printf("При скорости загрузки %.2f мегабит в секунду файл размером %.2f мегабайт загружается за %.2f секунды\n", 
+    printf("With speed of download %.2f мBit in second file with weight of %.2f мBait will download in %.2f seconds\n", 
     speed,weight,8*weight/speed);
 
 
